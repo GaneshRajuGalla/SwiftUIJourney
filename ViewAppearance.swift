95a5ac77726831c214feb3f1372097c20d9873ff
Ganesh Raju Galla  <61533653+GaneshRajuGalla@users.noreply.github.com>
2023-10-07 01:04:41 +0530

    Add files via upload

---
+++ ViewAppearance.swift
@@ -0,0 +1,64 @@
+//
+//  ContentView.swift
+//  ViewAppearance
+//
+//  Created by Ganesh on 07/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    // MARK: - Properties
+    @State private var isGray:Bool = false
+    
+    // MARK: - Body
+    
+    var body: some View {
+        ZStack{
+//            if isGray{
+//                Color(.gray)
+//                    .ignoresSafeArea()
+//            }else {
+//                Color(.black)
+//                    .ignoresSafeArea()
+//            }
+            Color(isGray ? .gray : .black)
+                .ignoresSafeArea()
+            
+            Button(String.changeColor) {
+                isGray.toggle()
+            }
+            .modifier(CustomButton(backGroundColot: .orange))
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
+
+// MARK: - ViewModifier
+
+struct CustomButton: ViewModifier{
+    let backGroundColot:Color
+    
+    func body(content: Content) -> some View {
+        content
+            .font(.headline)
+            .fontWeight(.bold)
+            .foregroundStyle(.white)
+            .padding()
+            .frame(maxWidth: .infinity)
+            .background(backGroundColot, in: Capsule())
+    }
+}
+
+
+// MARK: - String
+
+extension String{
+    static let changeColor = "Change Color"
+}
